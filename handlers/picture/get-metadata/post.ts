61b110188e6b0a54a8502369746bada9c5e66322
menthamin <menthamin@naver.com>
2023-08-06 20:12:19 +0900

    init index.html

--- handlers/picture/get-metadata/post.ts
+++ handlers/picture/get-metadata/post.ts
@@ -6,12 +6,12 @@ async function uploadImage(filePath: string) {
     // const form = new FormData();
     const base64Image = fs.readFileSync(filePath, { encoding: "base64" });
     // form.append("imageFile", fs.createReadStream(filePath));
-
     // form-data에 필요한 헤더 정보를 가져옵니다.
     // const formHeaders = form.getHeaders();
     try {
         const response = await axios.post("http://localhost:4000/picture", base64Image);
-        console.log("Image uploaded successfully:", response.data);
+        // console.log("Image uploaded successfully:", response.data);
+        console.log(response.data.body);
     } catch (error) {
         console.error("Error uploading image:", error);
     }
